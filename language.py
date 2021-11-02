6204b2404c18c0a91ac9e75389ad057418baa127
priyusha99 <sunkarapriyusha1999@gmail.com>
2021-11-02 11:23:33 +0530

    code for buildUniformProbs()

--- language.py
+++ language.py
@@ -130,7 +130,12 @@ Parameters: list of strs
 Returns: list of floats
 '''
 def buildUniformProbs(unigrams):
-    return
+    uniform_probabilities=[]
+    length=len(unigrams)
+    for i in unigrams:
+        prob=1/length
+        uniform_probabilities.append(prob)
+    return uniform_probabilities
 
 
 '''
@@ -330,7 +335,7 @@ if __name__ == "__main__":
     # test.week1Tests()
     # print("\n" + "#"*15 + " WEEK 1 OUTPUT " + "#" * 15 + "\n")
     # test.runWeek1()
-    test.testCountBigrams()
+    test.testBuildUniformProbs()
 
     ## Uncomment these for Week 2 ##
 """
